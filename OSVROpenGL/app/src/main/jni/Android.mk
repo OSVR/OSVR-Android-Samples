1ca8408d330a122095235ccd92e259c4ec795257
bell.jeremy@gmail.com <bell.jeremy@gmail.com>
2015-09-03 22:41:37 -0400

    converted main.cp to main.cpp

--- OSVROpenGL/app/src/main/jni/Android.mk
+++ OSVROpenGL/app/src/main/jni/Android.mk
@@ -47,7 +47,7 @@ include $(PREBUILT_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := native-activity
-LOCAL_SRC_FILES := main.c
+LOCAL_SRC_FILES := main.cpp
 LOCAL_LDLIBS    := -llog -landroid -lEGL -lGLESv1_CM
 LOCAL_STATIC_LIBRARIES := android_native_app_glue
 LOCAL_SHARED_LIBRARIES := osvrClient osvrClientKit osvrCommon osvrUtil usb1.0 gnustl_shared jsoncpp
--- OSVROpenGL/app/src/main/jni/main.cpp
+++ OSVROpenGL/app/src/main/jni/main.cpp
@@ -223,84 +223,87 @@ static void engine_handle_cmd(struct android_app* app, int32_t cmd) {
     }
 }
 
-/**
- * This is the main entry point of a native application that is using
- * android_native_app_glue.  It runs in its own thread, with its own
- * event loop for receiving input events and doing other things.
- */
-void android_main(struct android_app* state) {
-    struct engine engine;
+extern "C" {
+    /**
+     * This is the main entry point of a native application that is using
+     * android_native_app_glue.  It runs in its own thread, with its own
+     * event loop for receiving input events and doing other things.
+     */
+    void android_main(struct android_app *state) {
+        struct engine engine;
 
-    // Make sure glue isn't stripped.
-    app_dummy();
+        // Make sure glue isn't stripped.
+        app_dummy();
 
-    memset(&engine, 0, sizeof(engine));
-    state->userData = &engine;
-    state->onAppCmd = engine_handle_cmd;
-    state->onInputEvent = engine_handle_input;
-    engine.app = state;
+        memset(&engine, 0, sizeof(engine));
+        state->userData = &engine;
+        state->onAppCmd = engine_handle_cmd;
+        state->onInputEvent = engine_handle_input;
+        engine.app = state;
 
-    // Prepare to monitor accelerometer
-    engine.sensorManager = ASensorManager_getInstance();
-    engine.accelerometerSensor = ASensorManager_getDefaultSensor(engine.sensorManager,
-            ASENSOR_TYPE_ACCELEROMETER);
-    engine.sensorEventQueue = ASensorManager_createEventQueue(engine.sensorManager,
-            state->looper, LOOPER_ID_USER, NULL, NULL);
+        // Prepare to monitor accelerometer
+        engine.sensorManager = ASensorManager_getInstance();
+        engine.accelerometerSensor = ASensorManager_getDefaultSensor(engine.sensorManager,
+                                                                     ASENSOR_TYPE_ACCELEROMETER);
+        engine.sensorEventQueue = ASensorManager_createEventQueue(engine.sensorManager,
+                                                                  state->looper, LOOPER_ID_USER, NULL,
+                                                                  NULL);
 
-    if (state->savedState != NULL) {
-        // We are starting with a previous saved state; restore from it.
-        engine.state = *(struct saved_state*)state->savedState;
-    }
+        if (state->savedState != NULL) {
+            // We are starting with a previous saved state; restore from it.
+            engine.state = *(struct saved_state *) state->savedState;
+        }
 
-    // loop waiting for stuff to do.
+        // loop waiting for stuff to do.
 
-    while (1) {
-        // Read all pending events.
-        int ident;
-        int events;
-        struct android_poll_source* source;
+        while (1) {
+            // Read all pending events.
+            int ident;
+            int events;
+            struct android_poll_source *source;
 
-        // If not animating, we will block forever waiting for events.
-        // If animating, we loop until all events are read, then continue
-        // to draw the next frame of animation.
-        while ((ident=ALooper_pollAll(engine.animating ? 0 : -1, NULL, &events,
-                (void**)&source)) >= 0) {
+            // If not animating, we will block forever waiting for events.
+            // If animating, we loop until all events are read, then continue
+            // to draw the next frame of animation.
+            while ((ident = ALooper_pollAll(engine.animating ? 0 : -1, NULL, &events,
+                                            (void **) &source)) >= 0) {
 
-            // Process this event.
-            if (source != NULL) {
-                source->process(state, source);
-            }
+                // Process this event.
+                if (source != NULL) {
+                    source->process(state, source);
+                }
 
-            // If a sensor has data, process it now.
-            if (ident == LOOPER_ID_USER) {
-                if (engine.accelerometerSensor != NULL) {
-                    ASensorEvent event;
-                    while (ASensorEventQueue_getEvents(engine.sensorEventQueue,
-                            &event, 1) > 0) {
-                        LOGI("accelerometer: x=%f y=%f z=%f",
-                                event.acceleration.x, event.acceleration.y,
-                                event.acceleration.z);
+                // If a sensor has data, process it now.
+                if (ident == LOOPER_ID_USER) {
+                    if (engine.accelerometerSensor != NULL) {
+                        ASensorEvent event;
+                        while (ASensorEventQueue_getEvents(engine.sensorEventQueue,
+                                                           &event, 1) > 0) {
+                            LOGI("accelerometer: x=%f y=%f z=%f",
+                                 event.acceleration.x, event.acceleration.y,
+                                 event.acceleration.z);
+                        }
                     }
+                }
+
+                // Check if we are exiting.
+                if (state->destroyRequested != 0) {
+                    engine_term_display(&engine);
+                    return;
                 }
             }
 
-            // Check if we are exiting.
-            if (state->destroyRequested != 0) {
-                engine_term_display(&engine);
-                return;
-            }
-        }
+            if (engine.animating) {
+                // Done with events; draw next animation frame.
+                engine.state.angle += .01f;
+                if (engine.state.angle > 1) {
+                    engine.state.angle = 0;
+                }
 
-        if (engine.animating) {
-            // Done with events; draw next animation frame.
-            engine.state.angle += .01f;
-            if (engine.state.angle > 1) {
-                engine.state.angle = 0;
+                // Drawing is throttled to the screen update rate, so there
+                // is no need to do timing here.
+                engine_draw_frame(&engine);
             }
-
-            // Drawing is throttled to the screen update rate, so there
-            // is no need to do timing here.
-            engine_draw_frame(&engine);
         }
     }
 }
