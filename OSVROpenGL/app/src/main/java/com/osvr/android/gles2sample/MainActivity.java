04031ca199eb126b8d1b6fdcdefd7bb38c682fe1
JeroMiya <bell.jeremy@gmail.com>
2015-09-09 10:43:57 -0400

    License updates.

--- OSVROpenGL/app/src/main/java/com/osvr/android/gles2sample/MainActivity.java
+++ OSVROpenGL/app/src/main/java/com/osvr/android/gles2sample/MainActivity.java
@@ -1,4 +1,7 @@
 /*
+ * Copyright (C) 2015 Sensics, Inc. and contributors.
+ *
+ * Based on Android NDK samples, which are:
  * Copyright (C) 2007 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
